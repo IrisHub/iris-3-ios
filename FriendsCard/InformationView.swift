42bf4cb805dfbc09084c9932db0ce8aeca3116a1
Shalin Shah <shalinvs@gmail.com>
2020-09-26 16:37:50 -0700

    Build Version Bump by fastlane

---
+++ FriendsCard/InformationView.swift
@@ -0,0 +1,21 @@
+//
+//  InformationView.swift
+//  FriendsCard
+//
+//  Created by Shalin on 9/25/20.
+//  Copyright © 2020 Shalin. All rights reserved.
+//
+
+import SwiftUI
+
+struct InformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        InformationView()
+    }
+}
