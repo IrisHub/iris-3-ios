b84663467ee720a818d6deb0328bc912e1b53557
Shalin Shah <shalinvs@gmail.com>
2020-09-27 00:47:15 -0700

    New TestFlight !!! Build version bumped to 26

---
+++ FriendsCard/FOUNDERS/AccessCode.swift
@@ -0,0 +1,21 @@
+//
+//  AccessCode.swift
+//  FriendsCard
+//
+//  Created by Shalin on 9/26/20.
+//  Copyright © 2020 Shalin. All rights reserved.
+//
+
+import SwiftUI
+
+struct AccessCode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccessCode_Previews: PreviewProvider {
+    static var previews: some View {
+        AccessCode()
+    }
+}
